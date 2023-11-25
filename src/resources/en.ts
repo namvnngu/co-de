071bf8eee765b29913f2529210bf9f1f855b5e53
Nam Nguyen <nguyenvietnam2401@gmail.com>
2023-11-25 17:07:28 +1100

    feat: implement resource

---
+++ src/resources/en.ts
@@ -0,0 +1,5 @@
+import type { Resource } from './interface';
+
+export const en: Resource = {
+  AppName: 'CoDe',
+};
---
+++ src/resources/interface.ts
@@ -0,0 +1,5 @@
+export type Language = 'en';
+
+export type Resource = {
+  AppName: string;
+};
