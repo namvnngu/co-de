e19eedb9950dec70baff5d695bb7636e5093aabd
Nam Nguyen <nguyenvietnam2401@gmail.com>
2023-11-25 18:36:41 +1100

    refactor: move theme logo to a store

--- src/components/ThemeToggle.svelte
+++ src/components/ThemeToggle.svelte
@@ -1,62 +1,13 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-
   import Moon from '@/icons/Moon.svelte';
   import Sun from '@/icons/Sun.svelte';
-
-  import {
-    type Theme,
-    setThemeStyle,
-    getSystemTheme,
-    getLocalStorageTheme,
-    setLocalStorageTheme,
-    darkColorSchemeMatchMedia,
-  } from '@/utils/theme';
-
-  // VALUES
-  const initialLocalStorageTheme = getLocalStorageTheme();
-  let currentTheme: Theme = initialLocalStorageTheme
-    ? initialLocalStorageTheme
-    : getSystemTheme();
-  let mode: 'USER' | 'SYSTEM' = initialLocalStorageTheme ? 'USER' : 'SYSTEM';
-
-  // HANDLERS
-  function toggleTheme() {
-    if (mode !== 'USER') {
-      mode = 'USER';
-    }
-
-    const newTheme: Theme = currentTheme === 'LIGHT' ? 'DARK' : 'LIGHT';
-    currentTheme = newTheme;
-    setLocalStorageTheme(newTheme);
-    setThemeStyle(newTheme);
-  }
-
-  function setThemeOnMediaMatch(event: MediaQueryListEvent | MediaQueryList) {
-    const newTheme = event.matches ? 'DARK' : 'LIGHT';
-    currentTheme = newTheme;
-    setThemeStyle(newTheme);
-  }
-
-  // LIFECYCLE
-  onMount(() => {
-    setThemeStyle(currentTheme);
-
-    if (initialLocalStorageTheme) return;
-
-    darkColorSchemeMatchMedia.addEventListener('change', setThemeOnMediaMatch);
-    return () => {
-      darkColorSchemeMatchMedia.removeEventListener('change', setThemeOnMediaMatch);
-    };
-  });
-
-  // REACTIVE STATEMENTS
-  $: if (mode === 'USER' && !initialLocalStorageTheme) {
-    darkColorSchemeMatchMedia.removeEventListener('change', setThemeOnMediaMatch);
-  }
+  import { theme } from '@/stores/theme';
 </script>
 
-<button class="theme-toggle" on:click={toggleTheme}>
+<button
+  class="theme-toggle"
+  on:click={() => theme.set($theme === 'DARK' ? 'LIGHT' : 'DARK')}
+>
   <span class="option dark-option"><Moon /></span>
   <span class="option light-option"><Sun /></span>
 </button>
--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -1,4 +1,4 @@
-import type { Theme } from './theme';
+import type { Theme } from '@/stores/theme';
 
 type LocalStorage = {
   theme: Theme;
