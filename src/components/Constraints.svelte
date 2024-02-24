1f60637ab0e26d54fe0f998d08975d0480df6873
Nam Nguyen <nguyenvietnam2401@gmail.com>
2024-02-24 15:10:46 +1100

    feat: save value of constraints section into local storage

--- src/components/Constraints.svelte
+++ src/components/Constraints.svelte
@@ -1,6 +1,20 @@
-<script>
+<script lang="ts">
   import { resource } from '@/stores/resource';
   import TextArea from './TextArea.svelte';
+  import { getLocalStorage, setLocalStorage } from '@/utils/local-storage';
+
+  let constraints = getLocalStorage('constraints') ?? '';
+
+  function handleConstraintsChange(event: Event) {
+    const newConstraints = (event.target as HTMLTextAreaElement).value;
+    constraints = newConstraints;
+    setLocalStorage('constraints', newConstraints);
+  }
 </script>
 
-<TextArea id="constraints" title={$resource.Constraints} />
+<TextArea
+  id="constraints"
+  value={constraints}
+  title={$resource.Constraints}
+  on:input={handleConstraintsChange}
+/>
