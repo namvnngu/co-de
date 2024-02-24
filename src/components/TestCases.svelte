ff9fdcd563d5fdafd5b7d5f18ce800de89239517
Nam Nguyen <nguyenvietnam2401@gmail.com>
2024-02-24 15:11:02 +1100

    feat: save value of test cases section into local storage

--- src/components/TestCases.svelte
+++ src/components/TestCases.svelte
@@ -1,6 +1,20 @@
-<script>
+<script lang="ts">
   import { resource } from '@/stores/resource';
   import TextArea from './TextArea.svelte';
+  import { getLocalStorage, setLocalStorage } from '@/utils/local-storage';
+
+  let testCases = getLocalStorage('testCases') ?? '';
+
+  function handleTestCasesChange(event: Event) {
+    const newTestCases = (event.target as HTMLTextAreaElement).value;
+    testCases = newTestCases;
+    setLocalStorage('testCases', newTestCases);
+  }
 </script>
 
-<TextArea id="test-cases" title={$resource.TestCases} />
+<TextArea
+  id="test-cases"
+  value={testCases}
+  title={$resource.TestCases}
+  on:input={handleTestCasesChange}
+/>
